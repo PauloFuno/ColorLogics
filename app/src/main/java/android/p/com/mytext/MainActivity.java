02c4b114d2a975886efd1d975b2f5be5e0a1197e
pauloschmidt <paulosergiofuno@gmail.com>
2019-01-31 14:41:47 -0800

    % working

--- app/src/main/java/android/p/com/mytext/MainActivity.java
+++ app/src/main/java/android/p/com/mytext/MainActivity.java
@@ -1,11 +1,13 @@
 package android.p.com.mytext;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,12 +15,18 @@ public class MainActivity extends AppCompatActivity {
     private Button mDick;
     private ImageView mImageView;
     private EditText mValueInput;
+    private TextView mTotal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         onButton();
+
+
+
 
 
     }
@@ -35,6 +43,16 @@ public class MainActivity extends AppCompatActivity {
         mDick.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                mTotal = (TextView) findViewById(R.id.value_total);
+                String mTotalString = mTotal.getText().toString();
+                int mTotalInt = Integer.parseInt(mTotalString);
+
+                int goodX = mTotalInt * 50;
+                int good = goodX / 100;
+
+                int okX = mTotalInt * 70;
+                int ok = okX / 100;
+
                 mImageView = (ImageView) findViewById(R.id.image_bg);
                 getValue();
                 String stringInput = mValueInput.getText().toString();
@@ -42,12 +60,12 @@ public class MainActivity extends AppCompatActivity {
                 result = intInput + result;
 
 
-                if (result >= 700) {
+                if (result >= ok) {
 
                     int colorAccent = getResources().getColor(R.color.colorAccent);
                     mImageView.setBackgroundColor(colorAccent);
 
-                } else if (result >= 500 && result < 700) {
+                } else if (result >= good && result < ok) {
 
                     int colorPrimary = getResources().getColor(R.color.colorPrimary);
                     mImageView.setBackgroundColor(colorPrimary);
